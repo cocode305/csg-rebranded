312399f8cd5e5d2333c90829c33a8cb1a6c4c39f
xococo <eneluwecc@gmail.com>
2025-04-10 16:03:11 +0100

    Fixed the errors in the mobile nav menu

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ const navMenu = document.querySelector('.nav-menu');
 
 //Function to toggle the menu
 function toggleMenu(event) {
-    navMenu.classList.toggle('active');
+    navMenu.classList.toggle('toggleMenu');
     event.stopPropagation(); //Prevent click from propagating to document.
 }
 
 // Function to close the menu when clicking outside.
 function closeMenu(event) {
     if (!navMenu.contains(event.target) && !navToggleBtn.contains(event.target)) {
-        navMenu.classList.remove('active');
+        navMenu.classList.remove('toggleMenu');
     }
 }
 navToggleBtn.addEventListener('click', toggleMenu);
 
-closeNavMenu.addEventListener('click', () => navMenu.classList.remove('active'));
+closeNavMenu.addEventListener('click', () => navMenu.classList.remove('toggleMenu'));
 document.addEventListener('click', closeMenu);

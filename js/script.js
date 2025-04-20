3a16d60b0010b689074e914714f23e961163ad2b
xococo <eneluwecc@gmail.com>
2025-04-20 06:28:32 +0100

    Added Event function to open and close the mobile nav menu and search bar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 const navToggleBtn = document.querySelector('.nav-toggleBtn');
+const mobileSearchBtn = document.querySelector('.searchBtn')
+const mobileSearchBox = document.querySelector('.mobile-search');
 const closeNavMenu = document.querySelector('.close-btn');
-const navMenu = document.querySelector('.nav-menu');
+const navMenu = document.querySelector('.mobile-nav-menu');
 
 
 //Function to toggle the menu
 function toggleMenu(event) {
-    navMenu.classList.toggle('toggleMenu');
     event.stopPropagation(); //Prevent click from propagating to document.
+
+    // Close Search Box if open
+    mobileSearchBox.classList.remove('toggleMobileSearch');
+
+    navMenu.classList.toggle('toggleMenu');
+    
+}
+
+function toggleMobileSearchBox (event) {
+    event.stopPropagation();
+
+    // Close nav menu if open
+    navMenu.classList.remove('toggleMenu');
+
+    mobileSearchBox.classList.toggle('toggleMobileSearch');
+    
 }
 
 // Function to close the menu when clicking outside.
@@ -15,7 +32,18 @@ function closeMenu(event) {
         navMenu.classList.remove('toggleMenu');
     }
 }
-navToggleBtn.addEventListener('click', toggleMenu);
 
+function closeSearch (event) {
+    if (!mobileSearchBox.contains(event.target) && !mobileSearchBtn.contains(event.target)) {
+        mobileSearchBox.classList.remove('toggleMobileSearch');
+    }
+}
+
+// Events
+navToggleBtn.addEventListener('click', toggleMenu);
+mobileSearchBtn.addEventListener('click', toggleMobileSearchBox);
 closeNavMenu.addEventListener('click', () => navMenu.classList.remove('toggleMenu'));
-document.addEventListener('click', closeMenu);
+document.addEventListener('click', function(event) {
+    closeMenu(event);
+    closeSearch(event);
+});
